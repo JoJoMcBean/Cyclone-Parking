a0641ab86b9d77328d398a000f9756e919dbdd79
unknown <dgoel@iastate.edu>
2019-02-17 13:27:40 -0600

    added Post request

--- Backend/CycloneParking/src/main/java/com/example/demo/user_login/user_login.java
+++ Backend/CycloneParking/src/main/java/com/example/demo/user_login/user_login.java
@@ -21,7 +21,6 @@ import org.springframework.core.style.ToStringCreator;
 public class user_login {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "username")
     @NotFound(action = NotFoundAction.IGNORE)
     private String username;
@@ -52,18 +51,22 @@ public class user_login {
     }
 
     public String getPassword() {
+
         return this.password;
     }
 
     public void setPassword(String password) {
+
         this.password = password;
     }
 
-    public String getUser_type() {
+    public String getUser_type()
+    {
         return this.user_type;
     }
 
     public void setUser_type(String user_type) {
+
         this.user_type = user_type;
     }
 
@@ -71,7 +74,7 @@ public class user_login {
         return this.email;
     }
 
-    public void setEmail(String email) {
+        public void setEmail(String email) {
         this.email = email;
     }
 
--- Backend/CycloneParking/src/main/java/com/example/demo/user_login/user_loginController.java
+++ Backend/CycloneParking/src/main/java/com/example/demo/user_login/user_loginController.java
@@ -5,15 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@EnableWebMvc
 class user_loginController {
 
     @Autowired
@@ -22,11 +22,22 @@ class user_loginController {
     private final Logger logger = LoggerFactory.getLogger(user_loginController.class);
 
 
+    @RequestMapping(method = RequestMethod.POST, value = "/add/user")
+    public user_login createUser(@Valid @RequestBody user_login newUser) {
+        return user_loginRepository.save(newUser);
+    }
+
     @RequestMapping(method = RequestMethod.GET, path = "/users")
     public List<user_login> getAllUsers() {
         logger.info("Entered into Controller Layer");
         List<user_login> results = user_loginRepository.findAll();
         logger.info("Number of Records Fetched:" + results.size());
+        return results;
+    }
+    @RequestMapping(method = RequestMethod.GET, path = "/users/{username}")
+    public Optional<user_login> findUserByUsername(@PathVariable("username") String username) {
+        logger.info("Entered into Controller Layer");
+        Optional<user_login> results = user_loginRepository.findById(username);
         return results;
     }
 
